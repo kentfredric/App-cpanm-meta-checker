60b61d1e26c14505e9c0a2b0f5efe2a4f861b87e
Kent Fredric <kentfredric@gmail.com>
2014-03-31 03:28:45 +1300

    Build results of ee35148 (on master)

--- lib/App/cpanm/meta/checker/State/Duplicates.pm
+++ lib/App/cpanm/meta/checker/State/Duplicates.pm
@@ -12,11 +12,21 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 use Moo qw( has );
 use App::cpanm::meta::checker::State::Duplicates::Dist;
 
+
+
+
+
 has 'dists' => (
   is      => ro  =>,
   lazy    => 1,
   builder => sub { {} },
 );
+
+
+
+
+
+
 
 sub seen_dist_version {
   my ( $self, $dist, $version ) = @_;
@@ -26,11 +36,28 @@ sub seen_dist_version {
   return $self->dists->{$dist}->seen_version($version);
 }
 
+
+
+
+
+
+
+
+
 sub has_duplicates {
   my ( $self, $dist ) = @_;
   return unless exists $self->dists->{$dist};
   return $self->dists->{$dist}->has_duplicates;
 }
+
+
+
+
+
+
+
+
+
 
 sub reported_duplicates {
   my ( $self, $dist, $set_reported ) = @_;
@@ -38,6 +65,14 @@ sub reported_duplicates {
   return $self->dists->{$dist}->reported($set_reported) if @_ > 2;
   return $self->dists->{$dist}->reported();
 }
+
+
+
+
+
+
+
+
 
 sub duplicate_versions {
   my ( $self, $dist ) = @_;
@@ -60,6 +95,35 @@ App::cpanm::meta::checker::State::Duplicates - Data tracking for duplicate distr
 =head1 VERSION
 
 version 0.001001
+
+=head1 METHODS
+
+=head2 C<→seen_dist_version>
+
+  ->seen_dist_version( 'Dist-Name', '5.000' );
+
+=head2 C<→has_duplicates>
+
+  if ( $o->has_duplicates( 'Dist-Name' ) ) {
+    ...
+  }
+
+=head2 C<→reported_duplicates>
+
+  if ( not $o->reported_duplicates('Dist-Name') ) {
+    /* report dups  */
+    $o->reported_duplicates('Dist-Name', 1);
+  }
+
+=head2 C<→duplicate_versions>
+
+  for my $v ( $o->duplicate_versions('Dist-Name') ) {
+
+  }
+
+=head1 ATTRIBUTES
+
+=head2 C<dists>
 
 =head1 AUTHOR
 
